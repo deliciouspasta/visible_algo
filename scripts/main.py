2c820c4d802bf7e8f7ff3c921fe6969a1d216b62
deliciouspasta <deliciouspasta8989.gmail.com>
2022-02-20 22:38:57 +0900

    ファイル名変更

--- scripts/main.py
+++ scripts/main.py
@@ -1,88 +1,84 @@
-from tkinter import *
+#参考にしたサイト
+# https://www.reddit.com/r/learnpython/comments/776kd9/tkinterhow_can_i_destroyhide_the_root_window/
 import tkinter as tk
-import textbox
-
+from bubbleExe import bubble
+from mergeSort import mergeSortClass
+from binarySearch import binarySearchClass
+from binTree import binTreeClass
+from dijkstra import dijkstraClass
+from transition import TransitionRoutine
+from helper import helpClass
+from dummy import DummyApp
 class FrameBase(tk.Tk):
+
     def __init__(self):
         tk.Tk.__init__(self)
         self.geometry("800x480")
         self.frame = StartPageFrame(self)
-        self.frame.pack(expand = True, fill = "both")
+        self.frame.pack(expand=True, fill="both")
+        #self.attributes("-fullscreen", True)
 
-    def change(self,frame):
-        self.frame.pack_forget()
+    def change(self, frame):
+        self.frame.pack_forget() # delete currrent frame
+        #キャストではなくただの関数の呼び出しによるオブジェクトの代入
+        #self.StartPageFrame(self)=TransitionRoutine(Applist[r][c][0])↓?
+        #つまりはself.frame = TransitionRoutine(Applist[r][c][0])ということ？↓
         self.frame = frame(self)
-        self.frame.pack(expandd = True, fill = "both")
+        self.frame.pack(expand=True, fill="both") # make new frame
 
     def backToStart(self):
         self.frame.pack_forget()
         self.frame = StartPageFrame(self)
-        self.frame.pack(expand = True,fill = "both")
+        self.frame.pack(expand=True, fill="both")
 
+class StartPageFrame(tk.Frame):
+    def __init__(self, master=None, **kwargs):
+        tk.Frame.__init__(self, master, **kwargs)
+        master.title("Start Page")
 
-#class StartPageFrame(tk.Frame):
-    
+        self.grid(column=0, row=0, sticky=tk.NSEW)
 
+        self.Applist = [
+                    [ [bubble, "バブルソート"], [mergeSortClass, "マージソート"],[binarySearchClass, "二分探索"] ], 
+                    [[binTreeClass, "二分探索木"],[dijkstraClass, "ダイクストラ法"],[helpClass, "ヘルプ"] ]
+                    ]
 
+        lbl = tk.Label(master=self, text ="Start Page", font=("Migu 1M",14))
+        lbl.grid(column=0,row=0,sticky=tk.NW, padx=10)
+        btn = tk.Button(
+            master = self,
+            text="Close",
+            width = 5,
+            bg = "#dc143c",
+            fg = "#ffffff",
+            command=self.master.destroy)
+        btn.grid(column=2, row=0,sticky=tk.NE)
 
+        for r in range(1,3):
+            for c in range(3):
+                btn = tk.Button(
+                        master=self,
+                        wraplength=150,
+                        justify=tk.LEFT,
+                        text=self.Applist[r-1][c][1],
+                        font=("Migu 1M", 32),
+                        bg="#00ff7f",
+                        command=self.gotoApp(r-1,c))
+                btn.grid(column=c, row=r, padx=10, pady=10, sticky=tk.NSEW)
 
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
 
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
 
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
 
-root = tk.Tk()
+    def gotoApp(self,r,c):
+        return lambda :self.master.change(self.Applist[r][c][0])
 
-#ウィンドウサイズ
-root.geometry("600x600")
-
-#ウィンドウタイトル
-root.title("テスト")
-
-root.configure(background = "blue")
-
-mainFrame = tk.Frame(root,width = 600,height = 2000)
-mainFrame.grid(row = 0,column = 0)
-
-mainCanvas = tk.Canvas(
-    mainFrame,
-    width = 600,
-    height = 2000,
-    bg = "red",
-    scrollregion = (0,0,600,2000)
-)
-
-
-tb1 = textbox.input_box()
-tb1.make_box_i(mainCanvas)
-
-tb2 = textbox.input_box()
-tb2.make_box_i(mainCanvas)
-
-tb3 = textbox.input_box()
-tb3.make_box_i(mainCanvas)
-
-tb4 = textbox.input_box()
-tb4.make_box_i(mainCanvas)
-
-tb5 = textbox.input_box()
-tb5.make_box_i(mainCanvas)
-
-tb6 = textbox.input_box()
-tb6.make_box_i(mainCanvas)
-
-
-
-#スクロールバー
-vbar = tk.Scrollbar(mainFrame,orient = VERTICAL)
-vbar.pack(side = RIGHT,fill = Y)
-vbar.config(command = mainCanvas.yview)
-
-mainCanvas.config(width = 600,height = 2000)
-mainCanvas.config(yscrollcommand = vbar.set)
-
-mainCanvas.pack(side = LEFT,expand = True,fill = BOTH)
-
-
-
-
-
-root.mainloop()
+if __name__=="__main__":
+    app=FrameBase()
+    app.mainloop()
